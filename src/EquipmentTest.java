289246cc1995c261932662586930de4dd1e618ad
Aaron Craig <aaron.t.craig-1@ou.edu>
2019-02-11 17:31:11 -0600

    Added getter tests and toString test for equipment class

--- src/EquipmentTest.java
+++ src/EquipmentTest.java
@@ -8,9 +8,50 @@ public class EquipmentTest
     {
         // Use the default constructor
         String strg = "batman/5,2.0,3.0,A batshaped bat";
+        String[] tokens = strg.split(",");
+        String[] idinfo = tokens[0].split("/");
+        String name = idinfo[0];
+        int count = Integer.parseInt(idinfo[1]);
+        double totalWeight = Double.parseDouble(tokens[1]);
+        double totalPrice = Double.parseDouble(tokens[2]);
+        String description = tokens[3];
         Equipment batEquip = new Equipment(strg);
-        String name = 
-        Assert.assertEquals(strg.parse(0,5), batEquip.getName());
-
+        
+        Assert.assertEquals(name, batEquip.getName());
+        Assert.assertEquals(count, batEquip.getCount());
+        Assert.assertEquals(totalWeight, batEquip.getTotalWeight());
+        Assert.assertEquals(totalPrice, batEquip.getTotalPrice());
+        Assert.assertEquals(description, batEquip.getDescription());
+    }
+    public void toStringTest() throws AssertException
+    {
+    	Equipment equip1 = new Equipment("batman/5,2.0,3.0,A batshaped bat");
+    	String expected = "Name: batman, Number: 5, Weight: 2.00 lbs, Price: $3.00 - A batshaped bat";
+    	String actual = equip1.toString();
+    	
+    	Assert.assertEquals(expected, actual);
+    }
+    public void getNameTest() throws AssertException
+    {
+    	Equipment equip1 = new Equipment("batman/5,2.0,3.0,A batshaped bat");
+    	String expected = "batman";
+    	String actual = equip1.getName();
+    	
+    	Assert.assertEquals(expected, actual);
+    }
+    public void getCountTest() throws AssertException
+    {
+    	Equipment equip1 = new Equipment("batman/5,2.0,3.0,A batshaped bat");
+    	int expected = 5;
+    	int actual = equip1.getCount();
+    	
+    	Assert.assertEquals(expected, actual);
+    }
+    public void getTotalWeightTest() throws AssertException
+    {
+    	Equipment equip1 = new Equipment("batman/5,2.0,3.0,A batshaped bat");
+    	double expected = 2.0;
+    	double actual = equip1.getTotalWeight();
+    	Assert.assertEquals(expected, actual);
     }
 }
