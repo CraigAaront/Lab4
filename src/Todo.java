d9a058ba472bad17dc66a2e39e3cf7f502350785
Aaron Craig <aaron.t.craig-1@ou.edu>
2019-02-12 10:58:28 -0600

    Started zootest tests, and updated todo.java

--- src/Todo.java
+++ src/Todo.java
@@ -1,18 +1,30 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 011
+Last Modified_______: 2019-02-12
+OU ID_______________: 113365209
+OU EMAIL____________: aaron.t.craig-1@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Create the equals method for the equipment class
+2. Create the equals method for the animals class
+3. Create the tests for the animalTest.java
+4. Create the tests for equipmentTest.java
+5. Create the tests for zooTest.java
+6. Run, compile, and debug any issues
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 5
+2. 5
+3. 10
+4. 10
+5. 15
+6. 20
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 7
+2. 5
+3. 10
+4. 15
+5. 40
+6. 20
 */}
--- src/ZooTest.java
+++ src/ZooTest.java
@@ -1,4 +1,10 @@
 public class ZooTest
 {
-
+	public void testGetTotalHeight() throws AssertException
+	{
+		Animal ani1 = new Animal("blue", "tiger", 20.0, 20.0);
+		Animal ani2 = new Animal("red", "duck", 10.0, 10.0);
+		Animal ani3 = new Animal("green", "godzilla", 5.0, 5.0);
+		double expected = 0.0;
+	}
 }
