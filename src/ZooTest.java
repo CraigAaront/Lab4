92a13d5a4835f359f2d6fd4ad1a92d337f587260
Aaron Craig <aaron.t.craig-1@ou.edu>
2019-02-12 11:22:17 -0600

    Wrote testGetCapacity for zooTest.java

--- src/ZooTest.java
+++ src/ZooTest.java
@@ -15,6 +15,16 @@ public class ZooTest
 	}
 	public void testsGetCapacity() throws AssertException
 	{
-		
+		Animal ani1 = new Animal("blue", "tiger", 20.0, 20.0);
+		Animal ani2 = new Animal("red", "duck", 10.0, 10.0);
+		Animal ani3 = new Animal("green", "godzilla", 5.0, 5.0);
+		Zoo zoo1 = new Zoo(1);
+		zoo1.addAnimal(ani1);
+		zoo1.addAnimal(ani2);
+		zoo1.addAnimal(ani3);
+		int expected = 4;
+		int actual = zoo1.getCapacity();
+		Assert.assertEquals(expected, actual);
 	}
+	
 }
