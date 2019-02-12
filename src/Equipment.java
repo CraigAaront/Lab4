fcf7391a05ce84d9a20038c0e42013385b92bbb0
Aaron Craig <aaron.t.craig-1@ou.edu>
2019-02-12 11:19:03 -0600

    Wrote testGetTotalHeight method for zooTest.java

--- src/Equipment.java
+++ src/Equipment.java
@@ -125,7 +125,7 @@ public class Equipment
         // Check that all fields match (name, count, totalWeight, totalPrice, and description):
         boolean sameName = this.getName().equals(otherEq.getName());
         boolean sameCount = this.getCount() == otherEq.getCount();
-        boolean sameWeight = this.getTotalWeight() == otherEq.getCount();
+        boolean sameWeight = this.getTotalWeight() == otherEq.getTotalWeight();
         boolean samePrice = this.getTotalPrice() == otherEq.getTotalPrice();
         boolean sameDescription = this.getDescription().equals(otherEq.getDescription());
         return sameName && sameCount && sameWeight && samePrice && sameDescription;
--- src/ZooTest.java
+++ src/ZooTest.java
@@ -5,6 +5,16 @@ public class ZooTest
 		Animal ani1 = new Animal("blue", "tiger", 20.0, 20.0);
 		Animal ani2 = new Animal("red", "duck", 10.0, 10.0);
 		Animal ani3 = new Animal("green", "godzilla", 5.0, 5.0);
-		double expected = 0.0;
+		Zoo zoo1 = new Zoo(1);
+		double expected = 35.0;
+		zoo1.addAnimal(ani1);
+		zoo1.addAnimal(ani2);
+		zoo1.addAnimal(ani3);
+		double actual = zoo1.getTotalHeight();
+		Assert.assertEquals(expected, actual);
+	}
+	public void testsGetCapacity() throws AssertException
+	{
+		
 	}
 }
